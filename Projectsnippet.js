8b60571972a6ea4e5d68ad5171067c9afb92cb88
Frimpong Bright Adjei <owurakufrimpong772@gmail.com>
2025-07-14 23:59:31 +0000

    Update Projectsnippet.js
    Changed my first approach a little

--- Projectsnippet.js
+++ Projectsnippet.js
@@ -1,13 +1,13 @@
 import { motion } from 'framer-motion'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 const Projectsnippet = () => {
     return (
-        <div
-        className='projectsnippet'>
+        <div>
         <motion.div 
-          initial={{opacity:0,y:85}}
+          initial={{opacity:0,y:-85}}
           whileInView={{opacity:1,y:0}}
           transition={{duration:1.5}}
           className='projectsnippet'>
@@ -20,38 +20,23 @@ const Projectsnippet = () => {
                         initial={{opacity:0,x:100}}
                         whileInView={{opacity:1,x:0}}
                         transition={{duration:1}}
-                    >Pizza Order System</motion.li>
+                    ><header><h2>SHOPWICE</h2></header>
+                    <p>En errant dans des espaces numériques silencieux, nous tombons parfois sur des idées inattendues qui façonnent l'avenir. Qu'il s'agisse d'une simple étincelle ou d'une étrange coïncidence, ces instants méritent une place. Cet espace contient des histoires encore non écrites, des pensées à moitié formées et des pages en attente d’être remplies. Pour l’instant, respirez, faites défiler, et laissez les pixels parler.</p></motion.li>
+                      <div className="viewallpro">
+                    <Link className='viewallprolink' to={"/projects"}>view all</Link>
+
+                </div>
                     <motion.li 
                         initial={{opacity:0,x:100}}
                         whileInView={{opacity:1,x:0}}
                         transition={{duration:1}}
-                    >React Contact book</motion.li>
-                    <motion.li
-                        initial={{opacity:0,x:100}}
-                        whileInView={{opacity:1,x:0}}
-                        transition={{duration:1}}
-                    >Weather Website</motion.li>
-                    <motion.li
-                        initial={{opacity:0,x:100}}
-                        whileInView={{opacity:1,x:0}}
-                        transition={{duration:1,delay:0.4}}
-                     >Creative--Agency website</motion.li>
-                    <motion.li 
-                        initial={{opacity:0,x:100}}
-                        whileInView={{opacity:1,x:0}}
-                        transition={{duration:1,delay:0.4}}
-                    >__</motion.li>
-                    <motion.li
-                        initial={{opacity:0,x:100}}
-                        whileInView={{opacity:1,x:0}}
-                        transition={{duration:1,delay:0.4}}
-                    >__</motion.li>
+                    ><header><h2>Creative Agency</h2></header>
+                    <p>En errant dans des espaces numériques silencieux, nous tombons parfois sur des idées inattendues qui façonnent l'avenir. Qu'il s'agisse d'une simple étincelle ou d'une étrange coïncidence, ces instants méritent une place. Cet espace contient des histoires encore non écrites, des pensées à moitié formées et des pages en attente d’être remplies. Pour l’instant, respirez, faites défiler, et laissez les pixels parler.</p></motion.li>
+                
                 </ul>
+              
             </div>
-        <div className='seemoreprojects'>
-            <motion.button 
-               whileHover={{backgroundColor: "rgba(255, 238, 142, 0.775)",color:"rgb(173, 40, 49)"}}>SEE MORE</motion.button>
-        </div>
+       
         </motion.div>
             
         </div>
